57d4518af0a383b3beed6e5beb0b334f71894718
mohdraqif <48005159+mohdraqif@users.noreply.github.com>
2021-03-04 10:00:00 +0530

    Update Slideshow.js

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 export default class Slideshow extends Component {
   state = {
     clients:[
-      { "src": "https://i2.wp.com/artysan.co.in/wp-content/uploads/2018/06/cropped-CADD-NEW-1.jpg?zoom=1.5&w=3840&ssl=1" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/08/cropped-unnamed-1.jpg" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/12/images.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/08/cropped-dufl.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/08/cropped-campus.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/08/cropped-ladli-3.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/08/cropped-spark-2.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2019/06/cropped-blossom.png" },
-      { "src": "https://secureservercdn.net/160.153.137.167/mhq.97f.myftpupload.com/wp-content/uploads/2018/06/oceanview.png" }
+      { "src": "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_2x1.jpg" },
+      { "src": "https://lh3.googleusercontent.com/proxy/TVKgMeyJAFlLiW0QEh0kAzPOA-c9Z1BPFobrluooRILSKJjiXDhprpl6plcmLC-d8DwW5pYdxvtHEMIGlWNa1pFl5sOVvcMmAJm5KXvfnM6VnDxEkLsFJqG0JEjcN4559F1pcHJPtcSIhP90L7O7zw5zWLMK4LI" },
+      { "src": "https://miro.medium.com/max/2678/0*NChTo-XqLOxLabIW" },
+      { "src": "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_2x1.jpg" },
+      { "src": "https://lh3.googleusercontent.com/proxy/TVKgMeyJAFlLiW0QEh0kAzPOA-c9Z1BPFobrluooRILSKJjiXDhprpl6plcmLC-d8DwW5pYdxvtHEMIGlWNa1pFl5sOVvcMmAJm5KXvfnM6VnDxEkLsFJqG0JEjcN4559F1pcHJPtcSIhP90L7O7zw5zWLMK4LI" },
+      { "src": "https://miro.medium.com/max/2678/0*NChTo-XqLOxLabIW" },
+      { "src": "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_2x1.jpg" },
+      { "src": "https://lh3.googleusercontent.com/proxy/TVKgMeyJAFlLiW0QEh0kAzPOA-c9Z1BPFobrluooRILSKJjiXDhprpl6plcmLC-d8DwW5pYdxvtHEMIGlWNa1pFl5sOVvcMmAJm5KXvfnM6VnDxEkLsFJqG0JEjcN4559F1pcHJPtcSIhP90L7O7zw5zWLMK4LI" },
+      { "src": "https://miro.medium.com/max/2678/0*NChTo-XqLOxLabIW" }
     ]
   }
   render() {
